50e15d2a0735606f67c7f21f494a018d2e7be7c5
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-02-17 03:33:14 +0100

    user enity is done

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,7 +1,11 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Case } from '../../case/entities/case.entity';
 import { Subscription } from '../../subscription/entities/subscription.entity';
-
+export enum UserRole {
+  LAWYER = 'lawyer',
+  ADMIN = 'admin',
+  MANAGER ="manager",
+}
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
@@ -14,7 +18,7 @@ export class User {
   password: string;
 
   @Column({ default: 'client' })
-  role: 'lawyer' | 'admin' | 'manager';
+  role: UserRole.LAWYER | UserRole.ADMIN | UserRole.MANAGER;
 
   @OneToMany(() => Case, (caseEntity) => caseEntity.client)
   cases: Case[];
