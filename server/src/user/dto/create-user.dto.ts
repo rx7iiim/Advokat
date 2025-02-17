8822f1dd36dd7c09685ae5f8dccc830a64104614
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-02-17 03:37:47 +0100

    user enity is done

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsEnum(UserRole)
+  role: UserRole;
+}
