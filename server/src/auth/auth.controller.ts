f26f11bc99e15310cd5b7cfe825f09e8b961a06c
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-03-05 03:59:00 +0100

    got problems with google services tommorow the auth will be ready

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Post, Body, Query, Req, HttpStatus } from '@nestjs/common';
-import { UserService } from '../user/user.service';
-import { CreateUserDto } from 'src/user/dto/create-user.dto';
-import { User } from 'src/user/entities/user.entity';
+import { Controller, Post, Body, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-    private readonly authService:AuthService,
-  
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signup(
-    @Req() req: Request,
-    @Body() createUserDto: CreateUserDto,
-  ): Promise<User|null> {
-    const user = await this.authService.signup(req , createUserDto);
-    //const email= this.authService.decryptEmail(user.email)
-    //const Mail= await this.userRepository.findOne({where:{email}})
-    //if (Mail)
-    //await this.authService.verifyEmail(Mail.email , Mail.confirmationCode);
-    return user
+  async signup( @Req() req: Request,
+  @Body() createUserDto: CreateUserDto,) {
+    return this.authService.signup(createUserDto);
   }
 
-  //@Post('confirm-email')
-  //async confirmEmail(@Query('token') token: string) {
-   // return this.userService.confirmEmail(token);
+  @Post('verify-email')
+  async verifyEmail(@Body('email') email: string, @Body('code') code: string) {
+    return this.authService.verifyEmail(email, code);
   }
+}
