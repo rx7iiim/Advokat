4ca1d3d2af2c6a11bd26c1cf77ee6e9fd2d5f7c6
tahaBelaloui-ntm <tahabelaloui@gmail.com>
2025-04-24 18:31:24 +0000

    rewrote the calnedar page

--- front/src/app/user/[username]/agenda/compo.tsx
+++ front/src/app/user/[username]/agenda/compo.tsx
@@ -1,81 +1,273 @@
 'use client'
 import React from "react";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from 'react'
 import { useRouter } from "next/navigation";
-import Calendar2 from "../../../components/calander/Calendar2";
-import Button from "../../../components/button";
-import { FaUser } from "react-icons/fa";
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { Dialog, Transition } from '@headlessui/react';
+import { EventSourceInput } from '@fullcalendar/core/index.js';
+import FullCalendar from '@fullcalendar/react';
+import interactionPlugin from '@fullcalendar/interaction';
+import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid'
+import dayGridPlugin from '@fullcalendar/daygrid';
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import DayCalendar from "../../../components/calander/calendardate";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+interface Event {
+  id: number;
+  title: string;
+  start: Date | string;
+  allDay: boolean;
+}
 
- function AgendaPage() {
-
-  const [view, setView] = useState<"month" | "day">("month");
+function AgendaPage() {
   const router = useRouter();
-  const [username, setUsername ]= useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const [allEvents, setAllEvents] = useState<Event[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [idToDelete, setIdToDelete] = useState<number | null>(null);
+  const [newEvent, setNewEvent] = useState<Event>({
+    title: '',
+    start: '',
+    allDay: false,
+    id: 0
+  });
 
   useEffect(() => {
-      fetch(`${API_URL}/auth/session`, {
-        credentials: "include",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (!data.authenticated || !data.username) {
-            router.push("/login"); 
-          } else {
-            setUsername(data.username);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching session:", error);
+    fetch(`${API_URL}/auth/session`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.authenticated || !data.username) {
           router.push("/login");
-        });
-    }, [router]);
-    
-    if (!username) return <p>Loading...</p>;
+        } else {
+          setUsername(data.username);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching session:", error);
+        router.push("/login");
+      });
+  }, [router, API_URL]);
+
+  if (!username) return <p>Loading...</p>;
+
+  function handleDateClick(arg: { date: Date, allDay: boolean }) {
+    setNewEvent({ ...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime() });
+    setShowModal(true);
+  }
+
+  function handleDeleteModal(data: { event: { id: string } }) {
+    setShowDeleteModal(true);
+    setIdToDelete(Number(data.event.id));
+  }
+
+  function handleDelete() {
+    setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)));
+    setShowDeleteModal(false);
+    setIdToDelete(null);
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    });
+    setShowDeleteModal(false);
+    setIdToDelete(null);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewEvent({
+      ...newEvent,
+      title: e.target.value
+    });
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setAllEvents([...allEvents, newEvent]);
+    setShowModal(false);
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    });
+  }
 
   return (
     <div className="flex flex-row overflow-hidden min-h-screen bg-gray-100 text-gray-800 p-2">
-      <Sidebar />
-      <div className="invisible w-[260px] h-[100vh]" aria-hidden="true"></div>
-      {/* Main Content */}
-      <main className="flex-1 pl-3 pt-2 overflow-y-auto">
-        {/* Calendar */}
-        <div className="mt-0 bg-white p-5 rounded-lg shadow-md fle">
-          <div className="text-white flex items-center justify-center mb-6">
-            <button
-              className={`px-6 py-2 rounded-md text-lg font-semibold transition-all relative  text-gray-700 hover:text-blue-500 ${
-                view === "month" ? "text-blue-500 after:absolute after:left-0 after:bottom-0 after:w-full after:h-[3px] after:bg-blue-500" : ""
-              }`}
-              onClick={() => setView("month")}
-            >
-              Months
-            </button>
-            <button
-             className={`px-6 py-2 rounded-md text-lg font-semibold transition-all relative  text-gray-700 hover:text-blue-500 ${
-              view === "day" ? "text-blue-500 after:absolute after:left-0 after:bottom-0 after:w-full after:h-[3px] after:bg-blue-500" : ""
-            }`}
-              onClick={() => setView("day")}
-            >
-              Days
-            </button>
-
-            <button className=" absolute  right-16 px-2 py-3 rounded-md text-lg font-semibold transition-all bg-[linear-gradient(135deg,#0048ff,#00d4ff)] text-white p-3">
-              <div className="flex items-center justify-center">
-                add event <img src="/plus-circle-svgrepo-com (1).svg" alt="add event" className="w-6 h-6 ml-2" />
-              </div>
-            </button>
-          </div>
-
-          {/* Render Selected Calendar */}
-          <div className="w-full max-w-4xl">
-            {view === "month" ? <Calendar2 /> : <DayCalendar />}
-          </div>
+       
+       <Sidebar />
+       <div className="invisible w-[260px] h-[100vh]" aria-hidden="true"></div>
+      <main className="flex h-[100vh] w-[75%] flex-col items-center justify-between p-5">
+        <div className="w-full h-full">
+          <FullCalendar
+            plugins={[
+              dayGridPlugin,
+              interactionPlugin,
+              timeGridPlugin
+            ]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            events={allEvents as EventSourceInput}
+            nowIndicator={true}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dateClick={handleDateClick}
+            eventClick={handleDeleteModal}
+          />
         </div>
+
+        {/* Delete Event Modal */}
+        <Transition.Root show={showDeleteModal} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setShowDeleteModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                      <div className="sm:flex sm:items-start">
+                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                        </div>
+                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                          <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                            Delete Event
+                          </Dialog.Title>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                              Are you sure you want to delete this event?
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="button"
+                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                        onClick={handleDelete}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                        onClick={handleCloseModal}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Add Event Modal */}
+        <Transition.Root show={showModal} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={setShowModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                    <div>
+                      <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                        <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-5">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          Add Event
+                        </Dialog.Title>
+                        <form action="submit" onSubmit={handleSubmit}>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              name="title"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+                              value={newEvent.title}
+                              onChange={handleChange}
+                              placeholder="Title"
+                            />
+                          </div>
+                          <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                            <button
+                              type="submit"
+                              className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                              disabled={newEvent.title === ''}
+                            >
+                              Create
+                            </button>
+                            <button
+                              type="button"
+                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                              onClick={handleCloseModal}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
       </main>
     </div>
   );
